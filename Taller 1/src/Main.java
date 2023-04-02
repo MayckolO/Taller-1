f4ee96c0a1e51523c6152799165d71307ce7ab4a
MayckolO <mayckol.olivares@alumnos.ucn.cl>
2023-04-01 23:31:27 -0300

    Creación de líneas

--- Taller 1/src/Main.java
+++ Taller 1/src/Main.java
@@ -1,5 +1,47 @@
+import edu.princeton.cs.stdlib.StdDraw;
+
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Lineas();
+    }
+
+    public static void Lineas(){
+
+        double min = -1.0;
+        double max = 1.0;
+
+        StdDraw.setXscale(min,max);
+        StdDraw.setYscale(min,max);
+
+        StdDraw.enableDoubleBuffering();
+
+        Color[] colores = {Color.BLACK, Color.BLUE, Color.CYAN, Color.RED};
+
+        int contadorLineas = 0;
+
+        double x0 = min + (max- min) * Math.random();
+        double y0 = min + (max- min) * Math.random();
+
+        double x1 = min + (max- min) * Math.random();
+        double y1 = min + (max- min) * Math.random();
+
+        while (contadorLineas < 6){
+
+            int random = (int) (Math.random() * colores.length);
+            Color color = colores[random];
+            StdDraw.setPenColor(color);
+
+            StdDraw.line(x0, y0, x1, y1);
+            StdDraw.show();
+
+            contadorLineas++;
+
+            x0 = x0 + 0.025;
+            y0 = y0 + 0.025;
+            x1 = x1 + 0.025;
+            y1 = y1 + 0.025;
+        }
     }
 }
