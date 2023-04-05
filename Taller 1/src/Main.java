dced929270024d10076f8b5772e0f6e04c2612b5
MayckolO <mayckol.olivares@alumnos.ucn.cl>
2023-04-05 14:35:27 -0400

    Metodo rebote

--- Taller 1/src/Main.java
+++ Taller 1/src/Main.java
@@ -1,3 +1,4 @@
+import com.sun.source.doctree.SerialDataTree;
 import edu.princeton.cs.stdlib.StdDraw;
 
 import java.awt.*;
@@ -17,7 +18,7 @@ public class Main {
 
         StdDraw.enableDoubleBuffering();
 
-        Color[] colores = {Color.BLACK, Color.BLUE, Color.CYAN, Color.RED};
+        Color[] colores = {Color.BLACK,Color.BLUE,Color.CYAN,Color.RED,Color.DARK_GRAY,Color.GREEN,Color.MAGENTA,Color.ORANGE,Color.PINK,Color.YELLOW};
 
         int contadorLineas = 0;
 
@@ -32,9 +33,9 @@ public class Main {
             int random = (int) (Math.random() * colores.length);
             Color color = colores[random];
             StdDraw.setPenColor(color);
-
             StdDraw.line(x0, y0, x1, y1);
             StdDraw.show();
+            StdDraw.pause(400);
 
             contadorLineas++;
 
@@ -42,6 +43,37 @@ public class Main {
             y0 = y0 + 0.025;
             x1 = x1 + 0.025;
             y1 = y1 + 0.025;
+        }
+    }
+
+    public static void Rebote(){
+
+        double min = -1.0;
+        double max = 1.0;
+
+        StdDraw.setXscale(min,max);
+        StdDraw.setYscale(min,max);
+
+        StdDraw.enableDoubleBuffering();
+
+        double x = min + (max- min) * Math.random();
+        double y = min + (max- min) * Math.random();
+
+        double velocidadX = 0.012;
+        double velocidadY = 0.032;
+
+        while (true){
+
+            if(Math.abs(x + velocidadX) > 1.0){
+                velocidadX  = -velocidadX;
+            }
+
+            if(Math.abs(y + velocidadY) > 1.0){
+                velocidadY  = -velocidadY;
+            }
+
+            x += velocidadX;
+            y += velocidadY;
         }
     }
 }
