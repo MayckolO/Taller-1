08e9d9c6f87588e4500da0bc6968696d0c334091
MayckolO <mayckol.olivares@alumnos.ucn.cl>
2023-04-08 20:07:44 -0400

    Documentación JavaDoc

--- Taller 1/src/Main.java
+++ Taller 1/src/Main.java
@@ -3,48 +3,75 @@ import edu.princeton.cs.stdlib.StdDraw;
 
 import java.awt.*;
 
+/**
+ * Programa screensaver
+ *
+ * @author Mayckol Olivares Donoso
+ */
 public class Main {
+
+    /**
+     * The Main
+     */
     public static void main(String[] args) {
         panel();
     }
 
+    /**
+     * Subprograma para crear un panel
+     */
     public static void panel(){
+
+        //Rango de dibujo
         double min = -1.0;
         double max = 1.0;
 
+        //Definición de la escala del panel
         StdDraw.setXscale(min,max);
         StdDraw.setYscale(min,max);
 
+        //Evitar el parpadeo de la pantalla
         StdDraw.enableDoubleBuffering();
 
+        //Subprograma para generar las líneas en el panel
         lineas();
     }
 
+    /**
+     * Subprograma para crear líneas
+     */
     public static void lineas(){
 
+        //Rango de dibujo
         double min = -1.0;
         double max = 1.0;
 
+        //Definición de la escala del panel
         StdDraw.setXscale(min,max);
         StdDraw.setYscale(min,max);
 
+        //Evitar el parpadeo de la pantalla
         StdDraw.enableDoubleBuffering();
 
         Color[] colores = {Color.BLACK,Color.BLUE,Color.CYAN,Color.RED,Color.DARK_GRAY,Color.GREEN,Color.MAGENTA,Color.ORANGE,Color.PINK,Color.YELLOW};
 
+        //Generación por un par ordenado (x,y)
         double x0 = min + (max - min) * Math.random();
         double y0 = min + (max- min) * Math.random();
 
         double x1 = min + (max- min) * Math.random();
         double y1 = min + (max- min) * Math.random();
 
+        //Velocidad inicial
         double velocidadX0 = 0.09;
         double velocidadY0 = 0.03;
         double velocidadX1 = 0.09;
         double velocidadY1 = 0.03;
 
+        //Ciclo infinito
         while (true){
 
+            //Colisión
             if(Math.abs(x0 + velocidadX0) > 1.0){
                 velocidadX0  = -velocidadX0;
             }
@@ -61,24 +88,30 @@ public class Main {
                 velocidadY1  = -velocidadY1;
             }
 
+            //Selecciona el color de la linea al azar
             int random = (int) (Math.random() * colores.length);
             Color color = colores[random];
             StdDraw.setPenColor(color);
 
             int contador = 0;
 
+            //Ciclo para generar 6 líneas
             while (contador < 6){
+
+                //Actualización de posición
                 x0 += velocidadX0;
                 y0 += velocidadY0;
                 x1 += velocidadX1;
                 y1 += velocidadY1;
 
+                //Dibujar la linea
                 StdDraw.line(x0,y0,x1,y1);
                 StdDraw.show();
                 StdDraw.pause(100);
 
                 contador++;
             }
+            //Limpiar el panel
             StdDraw.clear();
         }
     }
